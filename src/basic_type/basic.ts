f57b83e9879023f3c4f1b2a90ef642a936db2f9b
Minosanty <minosantiago17@gmail.com>
2025-11-28 15:05:47 -0300

    interfas de productos

---
+++ src/basic_type/basic.ts
@@ -0,0 +1,12 @@
+// no es transpilada a JavaScript 
+interface Product {
+  name: string;
+  price: number;
+}
+
+const myProduct: Product = {
+  name: "Ferrari Roma Coupe V8 Biturbo 620cv",
+  price: 1073245,
+}
+
+console.log(myProduct)
